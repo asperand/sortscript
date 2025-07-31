62086d3d6203aaf309938945f326d5dd6cff6b98
Ryan <38592470+asperand@users.noreply.github.com>
2025-07-31 12:55:01 -0400

    Update subfolder_name_append.ps1

--- subfolder_name_append.ps1
+++ subfolder_name_append.ps1
@@ -1,9 +1,9 @@
-// A one-time use script that goes through every subfolder and appends that subfolder's name to the filename if it doesn't contain it.
+# A one-time use script that goes through every subfolder and appends that subfolder's name to the filename if it doesn't contain it.
 
-// Allow User to select dir
-// From Selected dir, get all subfolders
-// For each subfolder
-//     For each file in subfolder
-//       If not filename contains subfoldername
-//           Append "_subfoldername" to filename
-//
+# Allow User to select dir
+# From Selected dir, get all subfolders
+# For each subfolder
+#     For each file in subfolder
+#       If not filename contains subfoldername
+#           Append "_subfoldername" to filename
+#
